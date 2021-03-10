ed020a668f20535a71a270f4a85c5aadd6778ff2
adrian <46441883+adrian1612@users.noreply.github.com>
2021-03-10 13:37:04 +0800

    nag implement ako

--- MISDGit/Program.cs
+++ MISDGit/Program.cs
@@ -13,5 +13,24 @@ namespace MISDGit
             Console.WriteLine("Hello Worlds");
             Console.ReadLine();
         }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get { return DateTime.Now.Subtract(Birthday).Days / 365; } }
+            public DateTime Birthday { get; set; }
+            public Person()
+            {
+
+            }
+
+            public Person(string _Name, DateTime _Birthday)
+            {
+                Name = _Name;
+                Birthday = _Birthday;
+            }
+
+
+        }
     }
 }
