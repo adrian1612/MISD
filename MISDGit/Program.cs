4f8ce9813621b6f15b0aed1f444cb96ab6057055
adrian <46441883+adrian1612@users.noreply.github.com>
2021-03-10 13:31:32 +0800

    sss

--- MISDGit/Program.cs
+++ MISDGit/Program.cs
@@ -10,7 +10,7 @@ namespace MISDGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Worldsss");
             Console.ReadLine();
         }
     }
