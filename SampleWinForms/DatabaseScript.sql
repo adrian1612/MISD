a42d035d8419586d6acfd7e07ffb25c0ed6df7a0
adrian <adrian@BUCAL-PC>
2021-04-08 21:54:17 +0800

    update

---
+++ SampleWinForms/DatabaseScript.sql
@@ -0,0 +1,9 @@
+﻿CREATE DATABASE dbsample
+GO
+
+CREATE TABLE [dbo].[tbl_sample] (
+    [ID]    INT          IDENTITY (1, 1) NOT NULL,
+    [fname] VARCHAR (50) NULL,
+    [mn]    VARCHAR (50) NULL,
+    [lname] VARCHAR (50) NULL
+);
